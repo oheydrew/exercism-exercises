4ec3ae17051efc1744f386c83496357b6a2a826a
oheydrew <drew@oheydrew.me>
2018-08-01 10:52:01 +1000

    Ruby Hamming Iteration #1

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,10 +1,23 @@
-class HammingTest
-  def self.compute(strand_a, strand_b)
+class Hamming
+  class << self
+    def compute(strand_a, strand_b)
+      check_length(strand_a, strand_b)
 
+      hamming = 0
+      strand_a.each_char.with_index(0) do |char, index|
+        hamming += 1 if char != strand_b[index]
+      end
+
+      hamming
+    end
+
+    private
+    def check_length(strand_a, strand_b)
+      raise ArgumentError, 'Strands must be the same length' if strand_a.length != strand_b.length
+    end
   end
-
 end
 
 module BookKeeping
-  VERSION = 1
+  VERSION = 3
 end
