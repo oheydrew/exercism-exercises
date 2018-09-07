60fc9944e739310cfe2d2d148532b9a04cad2ada
oheydrew <drew@oheydrew.me>
2018-09-07 19:49:20 +1000

    Ruby Raindrops Iteration #1 - Refactor #2

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,7 +1,6 @@
 class Raindrops
   def self.convert(num)
-    raise ArgumentError, 'Input must be a number greater than zero' if num.zero?
-
+    check_input(num)
     conditions = { 3 => 'Pling', 5 => 'Plang', 7 => 'Plong' }
 
     output = ''
@@ -9,11 +8,13 @@ class Raindrops
       output += conditions[condition] if (num % condition).zero?
     end
 
-    if output != ''
-      output
-    else
-      num.to_s
-    end
+    return output if output != ''
+    num.to_s
+  end
+
+  private_class_method def self.check_input(input)
+    int_input = input.to_i
+    raise ArgumentError, 'Input must be a number greater than zero' if int_input.zero?
   end
 end
 
