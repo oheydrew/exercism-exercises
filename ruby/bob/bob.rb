b2e7cada6c28bb512180d109dc367818e1ca7402
oheydrew <drew@oheydrew.me>
2018-09-09 14:26:22 +1000

    Ruby Bob Iteration #1

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,5 +1,22 @@
 class Bob
-  def self.hey(remark)
-    'Whatever.'
+  def self.hey(remark_original)
+    # strip the original input
+    remark = remark_original.gsub(/\s+/, '')
+
+    # yelling if the remark matches it's upcase version, AND there's a capital letter
+    yelling = true if (remark == remark.upcase) && remark[/[A-Z]/]
+    question = true if remark.chars.last == '?'
+
+    if remark == ''
+      'Fine. Be that way!'
+    elsif yelling && question
+      'Calm down, I know what I\'m doing!'
+    elsif yelling
+      'Whoa, chill out!'
+    elsif question
+      'Sure.'
+    else
+      'Whatever.'
+    end
   end
 end
